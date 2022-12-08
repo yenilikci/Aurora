44b1989e7a1960b00b952ccf2b696969bac8bfb7
yenilikci <m.melihcelik017@outlook.com>
2022-12-08 23:11:27 +0300

    creating files reducers logic

--- src/store/slices/files/files.ts
+++ src/store/slices/files/files.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { UserFile } from '@/types/Files';
 
 export interface FilesState {
@@ -16,7 +16,35 @@ export const initialState: FilesState = {
 const filesSlice = createSlice({
   name: 'files',
   initialState,
-  reducers: {},
+  reducers: {
+    setFiles(state, action: PayloadAction<UserFile[]>) {
+      state.userFiles = action.payload;
+      state.activeFilesIds = [];
+    },
+
+    addActiveFile(state, action: PayloadAction<string>) {
+      state.activeFilesIds.push(action.payload);
+    },
+
+    removeActiveFile(state, action: PayloadAction<string>) {
+      state.activeFilesIds = state.activeFilesIds.filter((fileId) => fileId !== action.payload);
+    },
+
+    setEditorActiveFile(state, action: PayloadAction<string | null>) {
+      state.editorActiveFileId = action.payload;
+    },
+
+    updateFileCode(state, action: PayloadAction<{ fileId: string; newCode: string }>) {
+      const { fileId, newCode } = action.payload;
+      const { userFiles } = state;
+      let userFileToUpdate = userFiles.find((file) => file.id === fileId);
+      if (userFileToUpdate) {
+        userFileToUpdate.code = newCode;
+      }
+    },
+  },
 });
+
+export const { setFiles, addActiveFile, removeActiveFile, setEditorActiveFile, updateFileCode } = filesSlice.actions;
 
 export const filesReducer = filesSlice.reducer;
