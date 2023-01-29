c1163fae31de65d633267ecdffdd68082d8e010e
yenilikci <m.melihcelik017@outlook.com>
2023-01-29 12:01:37 +0300

    add codeeditorcontainer

---
+++ src/components/codeEditor/CodeEditorContainer/CodeEditorContainer.tsx
@@ -0,0 +1,69 @@
+import { AppBar, Tab, Tabs } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { useAppSelector, useAppDispatch } from '@/store/hooks';
+import { setEditorActiveFile } from '@/store/slices/files/files';
+import selectActiveFiles from '@/store/selectors/selectActiveFiles/selectActiveFiles';
+import CustomTabPanel from '@/components/codeEditor/CustomTabPanel/CustomTabPanel';
+import CustomTabLabel from '@/components/codeEditor/CustomTabLabel/CustomTabLabel';
+import { ChangeEvent } from 'react';
+
+const CodeEditorContainer = () => {
+  const dispatch = useAppDispatch();
+  const activeFiles = useAppSelector(selectActiveFiles);
+  const editorActiveFileId = useAppSelector((state) => state.files.editorActiveFileId);
+
+  const onTabClick = (event: ChangeEvent<{}>, tabPosition: number) => {
+    const activeFile = activeFiles[tabPosition];
+    if (activeFile.id !== editorActiveFileId) {
+      dispatch(setEditorActiveFile(activeFile.id));
+    }
+  };
+
+  const tabValue = editorActiveFileId
+    ? activeFiles.findIndex((activeFile) => activeFile.id === editorActiveFileId)
+    : 0;
+
+  if (!activeFiles.length) {
+    return <EmptyMessage>Select a file</EmptyMessage>;
+  }
+
+  return (
+    <CodeEditorContainerDiv>
+      <AppBar position='static' color='default'>
+        <Tabs
+          textColor='primary'
+          indicatorColor='primary'
+          variant='scrollable'
+          value={tabValue}
+          onChange={onTabClick}
+        >
+          {activeFiles.map(activeFile => {
+            const { id } = activeFile;
+            return <Tab key={id} label={<CustomTabLabel activeFile={activeFile} />} />;
+          })
+          }
+        </Tabs>
+        {activeFiles.map(activeFile => {
+          const { id } = activeFile;
+          return <CustomTabPanel key={id} activeFile={activeFile} editorActiveFileId={editorActiveFileId} />;
+        })}
+      </AppBar>
+    </CodeEditorContainerDiv>
+  );
+};
+
+const CodeEditorContainerDiv = styled('div')({
+  flex: 1,
+  height: '100%',
+  overflow: 'hidden'
+});
+
+const EmptyMessage = styled('div')(({ theme }) => ({
+  display: 'flex',
+  height: '100%',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: theme.font
+}));
+
+export default CodeEditorContainer;
